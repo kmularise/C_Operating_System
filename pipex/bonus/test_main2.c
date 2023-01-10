b97e33db8857113f6693cc4aa7554a719a135cff
kmularise <kmula222@gmail.com>
2023-01-10 20:49:12 +0900

    test: 테스트코드 작성

--- pipex/bonus/test_main2.c
+++ pipex/bonus/test_main2.c
@@ -20,32 +20,35 @@ int	open_file_and_control_cmd2(int pipes[2], char **argv, char **envp)
 	cmd_path = find_path(infile_arg[0], paths);
 	free(s);
 	ft_free_dptr(paths);
+	//printf("dfdasjaklkj");
 	if (execve(cmd_path, infile_arg, envp) == -1)
 		show_error("no such file or directory");
 	return (0);
 }
 
-int	open_file_and_control_cmd2(int pre_pipes[2], int next_pipes[2], char **argv, char **envp)
+int	control_cmd_and_write_file2(int pipes[2], char **argv, char **envp)
 {
-	char	**infile_arg;
-	char	*cmd_path;
+	int		fd1;
 	char	*s;
-	int		fd0;
 	char	**paths;
+	char	**outfile_arg;
+	char	*cmd_path;
 
-	dup2()
-	dup2(fd0, 0);//처음 start
-	dup2(pipes[1], 1);//처음 start
-	close(pipes[0]);//닫아줘야 함
-	close(pipes[1]);//필요 없음
-	close(fd0);//필요 없음
+	fd1 = open(argv[get_height(argv) - 1],
+			O_RDWR | O_CREAT | O_TRUNC, 0777);
+	dup2(fd1, 1);
+	dup2(pipes[0], 0);
+	close(pipes[0]);
+	close(pipes[1]);
+	close(fd1);
 	s = get_path_env(envp);
 	paths = ft_split(s, ':');
-	infile_arg = parse(argv[2]);
-	cmd_path = find_path(infile_arg[0], paths);
+	outfile_arg = parse(argv[get_height(argv) - 2]);
+	//cmd_path = find_path(outfile_arg[0], paths);
 	free(s);
 	ft_free_dptr(paths);
-	if (execve(cmd_path, infile_arg, envp) == -1)
+	//printf("fdljfklajklsdj");
+	if (execve(cmd_path, outfile_arg, envp) == -1)
 		show_error("no such file or directory");
 	return (0);
 }
@@ -55,7 +58,7 @@ int main(int argc, char *argv[], char *envp[])
 	int pipe1[2];
 	int pipe2[2];
 	pid_t	pid;
-	pid_t	pid2
+	pid_t	pid2;
 
 	if (argc != 6)
 		show_error("invalid input!");
@@ -75,13 +78,26 @@ int main(int argc, char *argv[], char *envp[])
 		pid2 = fork();
 		if (pid2 == -1)
 			show_error("child process creation failure!");
-		else if (pid2== 0)
+		else if (pid2 == 0)
 		{
+			dup2(pipe2[1], 1);
+			dup2(pipe1[0], 0);
+			close(pipe2[0]);
+			close(pipe1[1]);
+			char *s = get_path_env(envp);
+			char **paths = ft_split(s, ':');
+			char **infile_arg = parse(argv[3]);
+			char *cmd_path = find_path(infile_arg[0], paths);
+			free(s);
+			ft_free_dptr(paths);
+			printf("dfdfdfsfdsfdscxvxv");
+			if (execve(cmd_path, infile_arg, envp) == -1)
+				show_error("no such file or directory");
 
 		}
-		else if(pid2 != )
+		else
 		{
-
+			control_cmd_and_write_file2(pipe2, argv, envp);
 		}
 	}
 
