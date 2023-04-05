fdda6278438de19d54fbb526eacca5dfb8c82b5a
Yuijin Kim <yuikim@c5r9s1.42seoul.kr>
2023-04-05 19:21:09 +0900

    [feat] make common resource

--- philosophers/philo/philosophers.c
+++ philosophers/philo/philosophers.c
@@ -1,11 +1,11 @@
 #include "philosophers.h"
 
 void	*execute_thread_function(void *data){
-	t_thread_info	*thread_info;
+	t_common_resource	*common_resource;
 
-	thread_info = (t_thread_info *)data;
-	printf("thread okay? %d\n",thread_info->idx);
-	return ((void *)thread_info);
+	common_resource = (t_common_resource *)data;
+	printf("idx %d\n", common_resource->current_idx);
+	return ((void *)common_resource);
 }
 
 
@@ -46,33 +46,61 @@ t_thread_info	**create_thread_info_arr(char *philo_num)
 	return (info_arr);
 }
 
+t_common_resource	*create_common_resource(char *philo_num)
+{
+	t_common_resource	*common_resource;
+	int					*forks;
+	int					size;
+	int					i;
 
+	common_resource = malloc(sizeof(t_common_resource));
+	size = ft_atoi(philo_num);
+	forks = malloc(sizeof(char) * (size + 1));
+	i = -1;
+	while (++i < size)
+		forks[i] = AVAILABLE;
+	forks[size] = 0;
+	common_resource->forks = forks;
+	common_resource->info_arr = create_thread_info_arr(philo_num);
+	return (common_resource);
+}
 
 int	main(int argc, char **argv)
 {
-	pthread_t		*philosophers;//phillsopher - thread 1:1
-	t_thread_info	**info_arr;
+	pthread_t			*philosophers;//phillsopher - thread 1:1
+	// t_thread_info		**info_arr;
+	t_common_resource	*common_resource;
 	int				size;
 	int				thread_id;
 	int				status;
+	long long		count;
+	count = 0;
 	// if (argc != 5 && argc != 6)
 	// 	return (0);
 	philosophers = create_phillosophers(argv[1]);
-	info_arr = create_thread_info_arr(argv[1]);
-	int i;
+	// info_arr = create_thread_info_arr(argv[1]);
+
+	common_resource = create_common_resource(argv[1]);
+	pthread_mutex_t	idx_mutex;
+	int	i;
 	i = 0;
 	while (i < ft_atoi(argv[1]))
 	{
 		// printf("%d\n", i);
 		// printf("info idx : %d\n", info_arr[i]->idx);
-		thread_id = pthread_create(&philosophers[i], NULL, execute_thread_function, (void *)info_arr[i]);
-		i++;
-	}
-	i = 0;
-	while (i < ft_atoi(argv[1]))
-	{
+		// pthread_mutex_lock(&idx_mutex);
+		// common_resource->current_idx = i;
+		// pthread_mutex_unlock(&idx_mutex);
+		common_resource->current_idx = i;
+		thread_id = pthread_create(&philosophers[i], NULL, execute_thread_function, (void *)common_resource);
 		pthread_join(philosophers[i], (void **)&status);
 		i++;
 	}
+	i = 0;
+	// while (i < ft_atoi(argv[1]))
+	// {
+	// 	pthread_join(philosophers[i], (void **)&status);
+	// 	i++;
+	// }
 	// system("leaks a.out");
 }
--- philosophers/philo/philosophers.h
+++ philosophers/philo/philosophers.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "libft/libft.h"
 
+#define AVAILABLE 0;
+#define USED 1;
 // typedef enum e_status{
 
 // }	t_status;
@@ -15,8 +17,21 @@
 // } t_phillo;
 
 //철학자 한명당 정보 담기
+typedef enum e_status{
+	EATING,
+	SLEEP,
+	WAKEUP
+}	t_status;
+
 typedef struct s_thread_info{
 	int	idx;
 }	t_thread_info;
+
+typedef struct s_common_resource{
+	int	size;
+	int	*forks;
+	int	current_idx;
+	t_thread_info **info_arr;
+}	t_common_resource;
 
 #endif
